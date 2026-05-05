5707482bddea7dc959e3b5870ead78f905258c84
Pythons-fox <s9313195@gmail.com>
2026-05-05 19:58:14 +0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -4,8 +4,7 @@ from random import randint
 font.init()
 from time import time as timer
 
-window = display.set_mode((500, 600))
-window.fill((255,200,100))
+window = display.set_mode((400, 400))
 
 class game_sprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
@@ -19,12 +18,22 @@ class game_sprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y)) 
 
 class player(game_sprite):
-    def update(self):
+    def update_r(self):
             keys = key.get_pressed()
-            if keys[K_LEFT] and self.rect.x > 5:
-                self.rect.x -= self.speed
-            if keys[K_RIGHT] and self.rect.x < 630: 
-                self.rect.x += self.speed
+            if keys[K_w] and self.rect.y > 5:
+                self.rect.y -= self.speed
+            if keys[K_s] and self.rect.y < 350: 
+                self.rect.y += self.speed
+    def update_l(self):
+            keys = key.get_pressed()
+            if keys[K_UP] and self.rect.y > 5:
+                self.rect.y -= self.speed
+            if keys[K_DOWN] and self.rect.y < 350: 
+                self.rect.y += self.speed
+
+rocet640_r = player('rocet1.png', 20, 30, 4, 30, 50 ) 
+rocet640_l = player('rocet2.png', 350, 30, 4, 30, 50 )  
+boll640 = game_sprite('Boll.png', 200, 200, 3, 40, 40)
 
 cloick = time.Clock()
 game = True
@@ -34,6 +43,13 @@ while game == True:
         if e.type == QUIT:
             game = False 
 
+    rocet640_r.update_r()
+    rocet640_l.update_l()
+
+    window.fill((255,200,100))
+    rocet640_r.reset()
+    rocet640_l.reset()
+    boll640.reset()
     display.update()
     cloick.tick(60) 
 
