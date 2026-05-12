7625e6609665ebd12662dbe4560b60b1fa28a0b6
Pythons-fox <s9313195@gmail.com>
2026-05-12 19:28:59 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ from random import randint
 font.init()
 from time import time as timer
 
+finish = False
+
 window = display.set_mode((400, 400))
 
 class game_sprite(sprite.Sprite):
@@ -33,26 +35,48 @@ class player(game_sprite):
 
 rocet640_r = player('rocet1.png', 20, 30, 4, 30, 50 ) 
 rocet640_l = player('rocet2.png', 350, 30, 4, 30, 50 )  
-boll640 = game_sprite('Boll.png', 200, 200, 3, 40, 40)
+boll640 = game_sprite('Boll.png', 200, 90, 1, 40, 40)
 
 cloick = time.Clock()
 game = True
+
+speed_x = 2
+speed_y = 2
+
+font1 = font.Font(None, 35)
+Lose1 = font1.render('PLAYER 1 LOSE!', True, (255, 0, 0)) 
+Lose2 = font1.render('PLAYER 2 LOSE!', True, (255, 0, 0)) 
 
 while game == True:
     for e in event.get():
         if e.type == QUIT:
             game = False 
-
-    rocet640_r.update_r()
-    rocet640_l.update_l()
-
     window.fill((255,200,100))
-    rocet640_r.reset()
-    rocet640_l.reset()
-    boll640.reset()
-    display.update()
-    cloick.tick(60) 
+    
+    if finish != True:
+        boll640.rect.x += speed_x
+        boll640.rect.y += speed_y
+        if boll640.rect.y > 400 - 50 or boll640.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(rocet640_l, boll640) or sprite.collide_rect(rocet640_r, boll640):
+            speed_x *= -1
+            boll640.rect.x += speed_x
+            boll640.rect.y += speed_y
+        rocet640_r.update_r()
+        rocet640_l.update_l()
+        if boll640.rect.x < -50:
+            finish = True
+            window.blit(Lose1, (100,100))
+        if boll640.rect.x > 400:
+            finish = True
+            window.blit(Lose2, (160,160))
 
+        
+        rocet640_r.reset()
+        rocet640_l.reset()
+        boll640.reset()
+        display.update()
+        cloick.tick(60) 
 
     
 
